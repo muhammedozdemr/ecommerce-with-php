efae23657f4b7db86f0bf0511ccfbb0dbf8e2b69
muhammedozdemr <muhammedozdemr@outlook.com>
2021-02-14 12:19:37 +0300

    create my orders page

---
+++ siparislerim.php
@@ -0,0 +1,47 @@
+<?php include 'header.php'; ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="page-title-wrap">
+					<div class="page-title-inner">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="bigtitle">Sipariş Bilgilerim</div>
+							<p>Siparişlerimizin listesi</p>
+						</div>
+						
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="title-bg">
+			<div class="title">Sipariş Bilgileri</div>
+		</div>
+		
+		<div class="table-responsive">
+			<table class="table table-bordered chart">
+				<thead>
+					<tr>
+						<th>Sipariş No</th>
+						<th>Tarih</th>
+						<th>Tutar</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>	
+						<td>Some Camera</td>
+						<td>PR - 2</td>
+						<td>225883</td>
+						<td><button class="btn btn-primary xs">Detay</button></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		
+		<div class="spacer"></div>
+	</div>
+	
+	
+				<?php include 'footer.php'; ?>
