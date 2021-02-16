ecff05e8b9abfe1c8dc169624ca818eb7e90bd92
muhammedozdemr <muhammedozdemr@outlook.com>
2021-02-16 12:38:48 +0300

    Listing categories in the sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,25 +1,20 @@
 	<div class="col-md-3"><!--sidebar-->
 				<div class="title-bg">
-					<div class="title">Categories</div>
+					<div class="title">Kategoriler</div>
 				</div>
 				
 				<div class="categorybox">
+					
 					<ul>
-						<li><a href="category.htm">Women Accessories</a></li>
-						<li><a href="category.htm">Men Shoes</a></li>
-						<li><a href="category.htm">Gift Specials</a></li>
-						<li><a href="category.htm">Electronics</a>
-							<ul>
-								<li><a href="#">iPhone 4S</a></li>
-								<li><a href="#">Samsung Galaxy</a></li>
-								<li><a href="#">MacBook Pro 17"</a></li>
-							</ul>
-						</li>
-						<li><a href="category.htm">On sale</a></li>
-						<li><a href="category.htm">Summer Specials</a></li>
-						<li><a href="category.htm">Electronics</a></li>
-						<li class="lastone"><a href="category.htm">Unique Stuff</a></li>
+						<?php 
+					$kategorisor=$db->prepare("SELECT * FROM kategori ORDER BY kategori_sira ASC");
+					$kategorisor->execute(); 
+					while($kategoricek=$kategorisor->fetch(PDO::FETCH_ASSOC)){
+					?>
+						<li><a href="#"><?php echo $kategoricek['kategori_ad'] ?></a></li>
+						<?php } ?>
 					</ul>
+				
 				</div>
 				
 				<div class="ads">
