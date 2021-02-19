bdeed8a221ecc4001834576b3f265b2e005a383d
muhammedozdemr <muhammedozdemr@outlook.com>
2021-02-19 14:09:38 +0300

    Withdrawing information about us to the homepage

--- index.php
+++ index.php
@@ -50,15 +50,20 @@
 		<div class="row">
 			<div class="col-md-9"><!--Main content-->
 				<div class="title-bg">
-					<div class="title">About Shopping</div>
+					<div class="title">Hakkımızda Bilgi</div>
 				</div>
 				<p class="ct">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<?php 
+					$hakkimizdasor=$db->prepare("SELECT * FROM hakkimizda WHERE hakkimizda_id=:id");
+					$hakkimizdasor->execute(array(
+						'id' => 0
+					));
+
+					$hakkimizdacek=$hakkimizdasor->fetch(PDO::FETCH_ASSOC);
+					echo substr($hakkimizdacek['hakkimizda_icerik'],0,1000); ?>
 				</p>
-				<p class="ct">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				</p>
-				<a href="" class="btn btn-default btn-red btn-sm">Read More</a>
+				
+				<a href="hakkimizda" class="btn btn-default btn-red btn-sm">Devamını Oku</a>
 				
 				<div class="title-bg">
 					<div class="title">Lastest Products</div>
